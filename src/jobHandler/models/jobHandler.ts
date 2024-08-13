609da637c2b2f9dfb74ea6d283bd1b4b6b833b29
razbroc <razbroc@gmail.com>
2024-08-13 17:59:29 +0300

    feat: add configurable pooling job types

--- src/jobHandler/models/jobHandler.ts
+++ src/jobHandler/models/jobHandler.ts
@@ -2,19 +2,21 @@ import { Logger } from '@map-colonies/js-logger';
 import { inject, injectable } from 'tsyringe';
 import { IFindJobsRequest, ITaskResponse, TaskHandler as QueueClient } from '@map-colonies/mc-priority-queue';
 import { SERVICES } from '../../common/constants';
+import { IConfig } from 'config';
 
 
 @injectable()
 export class JobHandler {
   public constructor(@inject(SERVICES.LOGGER) private readonly logger: Logger,
-    @inject(SERVICES.QUEUE_CLIENT) private readonly queueClient: QueueClient
+    @inject(SERVICES.QUEUE_CLIENT) private readonly queueClient: QueueClient,
+    @inject(SERVICES.CONFIG) private readonly config: IConfig
   ) { }
 
   public async getPolyPartsTask() : Promise<ITaskResponse<IFindJobsRequest> | undefined> {
-    const ingestionJobTypes : string[] = ["raz"];
     const taskType = 'polygon-parts'
+    const pollingJobTypes = this.config.get<string[]>('forbiddenTypesForParallelIngesion');
 
-    for (const jobType of ingestionJobTypes){
+    for (const jobType of pollingJobTypes){
       this.logger.debug({ msg: `try to dequeue task of type "${taskType}" and job of type "${jobType}"`}, jobType, taskType);
       const task = await this.queueClient.dequeue<IFindJobsRequest>(jobType, taskType);
       if (task !== null ){
