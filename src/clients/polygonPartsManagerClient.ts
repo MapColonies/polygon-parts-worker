618f1bbe4a09594a4d05d35fa9fdaea0f9fd7c17
razbroc <razbroc@gmail.com>
2024-09-23 19:21:56 +0300

    feat: add zod validation

---
+++ src/clients/polygonPartsManagerClient.ts
@@ -0,0 +1,26 @@
+import { HttpClient, IHttpRetryConfig } from "@map-colonies/mc-utils";
+import { inject, injectable } from "tsyringe";
+import config from 'config';
+import { Logger } from "@map-colonies/js-logger";
+import { Tracer } from "@opentelemetry/api";
+import { PolygonPartsPayload } from "@map-colonies/mc-model-types";
+import { withSpanAsyncV4 } from "@map-colonies/telemetry";
+import { SERVICES } from "../common/constants";
+
+@injectable()
+export class PolygonPartsManagerClient extends HttpClient {
+    public constructor(@inject(SERVICES.LOGGER) protected readonly logger: Logger, @inject(SERVICES.TRACER) public readonly tracer: Tracer) {
+        super(logger, config.get<string>('polygonPartsManager.baseurl'),
+            'PolygonPartsManager',
+            config.get<IHttpRetryConfig>('server.httpRetry'),
+            config.get<boolean>('server.disableHttpClientLogs'))
+    }
+
+
+    @withSpanAsyncV4
+    public async createNewPolyParts(requestBody: PolygonPartsPayload): Promise<void> {
+        const newPolyPartsUrl = `/polygonParts`;
+        this.logger.info({ msg: `sending new layer request` }, requestBody.productId);
+        await this.post(newPolyPartsUrl, requestBody);
+    }
+}
--- src/models/newLayerHandler.ts
+++ src/models/newLayerHandler.ts
@@ -1,22 +1,36 @@
-import { injectable } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 import { IJobResponse } from "@map-colonies/mc-priority-queue";
 import { HttpClient } from "@map-colonies/mc-utils"
 import { PolygonPart, PolygonPartsPayload, ProductType } from "@map-colonies/mc-model-types";
+import { z } from "zod";
 import { JobHandler } from "../common/interfaces";
+import { newRequestBodySchema } from "../schemas/polyPartsManager.schema"
+import { PolygonPartsManagerClient } from "../clients/polygonPartsManagerClient";
 
 @injectable()
 export class NewJobHandler implements JobHandler {
+  public constructor(
+    @inject(PolygonPartsManagerClient) private readonly polygonPartsManager: PolygonPartsManagerClient
+  ) { }
 
   public async processJob(job: IJobResponse<PolygonPart[], unknown>): Promise<void> {
 
 
     //add zod to validate job.internalId and job.productType
-    const requestBody: PolygonPartsPayload = {
+    const requestBody = {
       productId: job.id,
-      productType: job.productType as ProductType,
+      productType: job.productType,
       catalogId: job.internalId,
       productVersion: job.version,
       partsData: job.parameters,
+    };
+    const validationResult = newRequestBodySchema.safeParse(requestBody);
+    if (validationResult.success) {
+      const polyData: PolygonPartsPayload = validationResult.data;
+      
+      console.log("Valid PolyData:", polyData);
+    } else {
+      throw Error("unvalid request")
     }
   }
 
