c27612cee5779d119d743ffe95d35d9c6a5f60b7
razbroc <razbroc@gmail.com>
2024-09-24 11:44:33 +0300

    docs: idea for factory design

--- src/models/handlersFactory.ts
+++ src/models/handlersFactory.ts
@@ -1,13 +1,31 @@
+import { Logger } from '@map-colonies/js-logger';
+import { PolygonPartsManagerClient } from '../clients/polygonPartsManagerClient';
+import { JobHandler } from '../common/interfaces';
+import { NewJobHandler } from './newLayerHandler';
 
-import { Logger } from "@map-colonies/js-logger";
-import { PolygonPartsManagerClient } from "../clients/polygonPartsManagerClient";
-import { JobHandler } from "../common/interfaces";
-import { NewJobHandler } from "./newLayerHandler"
-
-  export function initJobHandler(jobHandlerType: string, logger: Logger, polygonPartsManager: PolygonPartsManagerClient): JobHandler {
-    switch (jobHandlerType) {
-      case ('Ingestion_New'):
-        return new NewJobHandler(logger, polygonPartsManager);
-    }
-    throw new Error(`Bad request,${jobHandlerType} job type is invalid`)
+export function initJobHandler(jobHandlerType: string, logger: Logger, polygonPartsManager: PolygonPartsManagerClient): JobHandler {
+  switch (jobHandlerType) {
+    case 'Ingestion_New':
+      return new NewJobHandler(logger, polygonPartsManager);
   }
+  throw new Error(`Bad request,${jobHandlerType} job type is invalid`);
+}
+
+//Ofer's suggestion
+// class Handler {
+//   constructor(logger: Logger) {}
+// public handle(data:string, ): string {
+//   return data;
+// }
+// }
+
+// function handlersFactory(container: DependencyContainer): Map<string, Handler> {
+//   const handler = container.resolve<Handler>('handler')
+//   const map = new Map<string, Handler>()
+//   map.set('newJobType', handler)
+//   return map;
+// }
+
+// const handlers = handlersFactory()
+
+// const handler = handlers.get('newJobType')
