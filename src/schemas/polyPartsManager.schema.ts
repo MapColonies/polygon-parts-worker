e875e21517760adc719a941d5b24c5ad160e4bdf
razbroc <razbroc@gmail.com>
2024-09-24 11:44:51 +0300

    style: lint

--- src/schemas/polyPartsManager.schema.ts
+++ src/schemas/polyPartsManager.schema.ts
@@ -1,14 +1,15 @@
 import { z } from 'zod';
-import { ProductType, PolygonPart } from "@map-colonies/mc-model-types";
+import { ProductType, PolygonPart } from '@map-colonies/mc-model-types';
 import { extendZodWithOpenApi } from 'zod-openapi';
 
 extendZodWithOpenApi(z);
 
-export const newRequestBodySchema = z.object({
+export const newRequestBodySchema = z
+  .object({
     productId: z.string(),
     productType: z.nativeEnum(ProductType),
     catalogId: z.string(),
     productVersion: z.string(),
     partsData: z.custom<PolygonPart>().array(),
-})
-    .openapi({ description: 'Poly Parts Manger "new" request schema', ref: 'newRequest' });
+  })
+  .openapi({ description: 'Poly Parts Manger "new" request schema', ref: 'newRequest' });
