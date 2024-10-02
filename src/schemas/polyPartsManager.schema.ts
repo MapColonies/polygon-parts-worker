8fee10347b8c2021a22b989cf4c86dbbac588694
razbroc <razbroc@gmail.com>
2024-10-02 09:54:37 +0300

    style:lint

--- src/schemas/polyPartsManager.schema.ts
+++ src/schemas/polyPartsManager.schema.ts
@@ -1,11 +1,10 @@
 import { z } from 'zod';
 import { ProductType, PolygonPart } from '@map-colonies/mc-model-types';
 
-export const ingestionNewRequestBodySchema = z
-  .object({
-    productId: z.string(),
-    productType: z.nativeEnum(ProductType),
-    catalogId: z.string(),
-    productVersion: z.string(),
-    partsData: z.custom<PolygonPart>().array(),
-  });
+export const ingestionNewRequestBodySchema = z.object({
+  productId: z.string(),
+  productType: z.nativeEnum(ProductType),
+  catalogId: z.string(),
+  productVersion: z.string(),
+  partsData: z.custom<PolygonPart>().array(),
+});
