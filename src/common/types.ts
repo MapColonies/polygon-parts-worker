3c8e8ce2e1526bf2630358a497ca09d31b00e7da
RonitKissis <ronitkisis1997@gmail.com>
2025-04-03 17:12:17 +0300

    fix: remove file

--- src/common/types.ts
+++
@@ -1,8 +0,0 @@
-export type ReplaceValuesOfType<T, VFrom, VTo> = {
-  [K in keyof T]: T[K] extends VFrom ? VTo : T[K];
-};
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type NullableRecordValues<T extends Record<PropertyKey, any>> = {
-  [K in keyof T]-?: T[K] extends NonNullable<T[K]> ? T[K] : Exclude<T[K] | null, undefined>;
-};
