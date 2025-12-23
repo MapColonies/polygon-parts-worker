b2ba4924f66a2962f29a64d9c207bb4edc05d4ce
almog8k <almog8k@gmail.com>
2025-12-23 13:48:56 +0200

    fix: restore and expand failed validation test cases in validationTaskFlow

--- tests/integration/validationTaksFlow/validationTaskFlow.cases.ts
+++ tests/integration/validationTaksFlow/validationTaskFlow.cases.ts
@@ -27,100 +27,100 @@ export const failedValidationTestCases: FailedValidationTestCase[] = [
       smallHolesCount: 0,
     },
     expectedErrorsCount: { ...defaultExpectedErrorsCount, metadata: 1 },
-    // },
-    // {
-    //   description: 'Vertices error',
-    //   shapefilePath: '/invalid/vertices_error/3205_vertices/ShapeMetadata.shp',
-    //   ppManagerValidationResult: {
-    //     parts: [],
-    //     smallHolesCount: 0,
-    //   },
-    //   expectedErrorsCount: { ...defaultExpectedErrorsCount, vertices: 1 },
-    // },
-    // {
-    //   description: 'Geometry validity error(Self-intersecting)',
-    //   shapefilePath: '/invalid/geometry_validity_error/self_intersecting/ShapeMetadata.shp',
-    //   ppManagerValidationResult: {
-    //     parts: [{ id: '1', errors: ['Geometry_Validity'] }],
-    //     smallHolesCount: 0,
-    //   },
-    //   expectedErrorsCount: { ...defaultExpectedErrorsCount, geometryValidity: 1 },
-    // },
-    // {
-    //   description: 'Small geometries error',
-    //   shapefilePath: '/invalid/for_pp_invalid_mock/ShapeMetadata.shp',
-    //   ppManagerValidationResult: {
-    //     parts: [{ id: '1', errors: ['Small_Geometry'] }],
-    //     smallHolesCount: 0,
-    //   },
-    //   expectedErrorsCount: { ...defaultExpectedErrorsCount, smallGeometries: 1 },
-    // },
-    // {
-    //   description: 'Small holes error',
-    //   shapefilePath: '/invalid/for_pp_invalid_mock/ShapeMetadata.shp',
-    //   ppManagerValidationResult: {
-    //     parts: [{ id: '1', errors: ['Small_Holes'] }],
-    //     smallHolesCount: 100,
-    //   },
-    //   expectedErrorsCount: { ...defaultExpectedErrorsCount, smallHoles: 1 },
-    // },
-    // {
-    //   description: 'Resolution error',
-    //   shapefilePath: '/invalid/for_pp_invalid_mock/ShapeMetadata.shp',
-    //   ppManagerValidationResult: {
-    //     parts: [{ id: '1', errors: ['Resolution'] }],
-    //     smallHolesCount: 0,
-    //   },
-    //   expectedErrorsCount: { ...defaultExpectedErrorsCount, resolution: 1 },
-    // },
-    // {
-    //   description: 'Unknown error',
-    //   shapefilePath: '/invalid/for_pp_invalid_mock/ShapeMetadata.shp',
-    //   ppManagerValidationResult: {
-    //     parts: [{ id: '1', errors: ['Unknown'] }],
-    //     smallHolesCount: 0,
-    //   },
-    //   expectedErrorsCount: { ...defaultExpectedErrorsCount, unknown: 1 },
-    // },
-    // {
-    //   description: 'Multiple errors(metadata+vertices)',
-    //   shapefilePath: '/invalid/metadata_vertices_error/ShapeMetadata.shp',
-    //   ppManagerValidationResult: {
-    //     parts: [],
-    //     smallHolesCount: 50,
-    //   },
-    //   expectedErrorsCount: {
-    //     ...defaultExpectedErrorsCount,
-    //     metadata: 1,
-    //     vertices: 1,
-    //   },
-    // },
-    // {
-    //   description: 'Multiple errors(PolygonPartsManager errors)',
-    //   shapefilePath: '/invalid/for_pp_invalid_mock/ShapeMetadata.shp',
-    //   ppManagerValidationResult: {
-    //     parts: [{ id: '1', errors: ['Resolution', 'Small_Geometry', 'Small_Holes', 'Geometry_Validity', 'Unknown'] }],
-    //     smallHolesCount: 200,
-    //   },
-    //   expectedErrorsCount: {
-    //     ...defaultExpectedErrorsCount,
-    //     resolution: 1,
-    //     smallGeometries: 1,
-    //     geometryValidity: 1,
-    //     smallHoles: 1,
-    //     unknown: 1,
-    //   },
-    // },
-    // {
-    //   description: '2 metadata errors in 2 features',
-    //   shapefilePath: '/invalid/metadata_error/2_features_with_invalid_date/ShapeMetadata.shp',
-    //   ppManagerValidationResult: {
-    //     parts: [],
-    //     smallHolesCount: 0,
-    //   },
-    //   expectedErrorsCount: {
-    //     ...defaultExpectedErrorsCount,
-    //     metadata: 2,
-    //   },
+  },
+  {
+    description: 'Vertices error',
+    shapefilePath: '/invalid/vertices_error/3205_vertices/ShapeMetadata.shp',
+    ppManagerValidationResult: {
+      parts: [],
+      smallHolesCount: 0,
+    },
+    expectedErrorsCount: { ...defaultExpectedErrorsCount, vertices: 1 },
+  },
+  {
+    description: 'Geometry validity error(Self-intersecting)',
+    shapefilePath: '/invalid/geometry_validity_error/self_intersecting/ShapeMetadata.shp',
+    ppManagerValidationResult: {
+      parts: [{ id: '1', errors: ['Geometry_Validity'] }],
+      smallHolesCount: 0,
+    },
+    expectedErrorsCount: { ...defaultExpectedErrorsCount, geometryValidity: 1 },
+  },
+  {
+    description: 'Small geometries error',
+    shapefilePath: '/invalid/for_pp_invalid_mock/ShapeMetadata.shp',
+    ppManagerValidationResult: {
+      parts: [{ id: '1', errors: ['Small_Geometry'] }],
+      smallHolesCount: 0,
+    },
+    expectedErrorsCount: { ...defaultExpectedErrorsCount, smallGeometries: 1 },
+  },
+  {
+    description: 'Small holes error',
+    shapefilePath: '/invalid/for_pp_invalid_mock/ShapeMetadata.shp',
+    ppManagerValidationResult: {
+      parts: [{ id: '1', errors: ['Small_Holes'] }],
+      smallHolesCount: 100,
+    },
+    expectedErrorsCount: { ...defaultExpectedErrorsCount, smallHoles: 1 },
+  },
+  {
+    description: 'Resolution error',
+    shapefilePath: '/invalid/for_pp_invalid_mock/ShapeMetadata.shp',
+    ppManagerValidationResult: {
+      parts: [{ id: '1', errors: ['Resolution'] }],
+      smallHolesCount: 0,
+    },
+    expectedErrorsCount: { ...defaultExpectedErrorsCount, resolution: 1 },
+  },
+  {
+    description: 'Unknown error',
+    shapefilePath: '/invalid/for_pp_invalid_mock/ShapeMetadata.shp',
+    ppManagerValidationResult: {
+      parts: [{ id: '1', errors: ['Unknown'] }],
+      smallHolesCount: 0,
+    },
+    expectedErrorsCount: { ...defaultExpectedErrorsCount, unknown: 1 },
+  },
+  {
+    description: 'Multiple errors(metadata+vertices)',
+    shapefilePath: '/invalid/metadata_vertices_error/ShapeMetadata.shp',
+    ppManagerValidationResult: {
+      parts: [],
+      smallHolesCount: 50,
+    },
+    expectedErrorsCount: {
+      ...defaultExpectedErrorsCount,
+      metadata: 1,
+      vertices: 1,
+    },
+  },
+  {
+    description: 'Multiple errors(PolygonPartsManager errors)',
+    shapefilePath: '/invalid/for_pp_invalid_mock/ShapeMetadata.shp',
+    ppManagerValidationResult: {
+      parts: [{ id: '1', errors: ['Resolution', 'Small_Geometry', 'Small_Holes', 'Geometry_Validity', 'Unknown'] }],
+      smallHolesCount: 200,
+    },
+    expectedErrorsCount: {
+      ...defaultExpectedErrorsCount,
+      resolution: 1,
+      smallGeometries: 1,
+      geometryValidity: 1,
+      smallHoles: 1,
+      unknown: 1,
+    },
+  },
+  {
+    description: '2 metadata errors in 2 features',
+    shapefilePath: '/invalid/metadata_error/2_features_with_invalid_date/ShapeMetadata.shp',
+    ppManagerValidationResult: {
+      parts: [],
+      smallHolesCount: 0,
+    },
+    expectedErrorsCount: {
+      ...defaultExpectedErrorsCount,
+      metadata: 2,
+    },
   },
 ];
