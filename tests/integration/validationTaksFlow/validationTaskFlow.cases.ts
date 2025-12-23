0d8c9e34e7e8832df0138ea5ca6da3e0da5625f3
almog8k <almog8k@gmail.com>
2025-12-23 09:37:54 +0200

    test: validation task workflow integration

---
+++ tests/integration/validationTaksFlow/validationTaskFlow.cases.ts
@@ -0,0 +1,126 @@
+import { PolygonPartsChunkValidationResult } from '@map-colonies/raster-shared';
+import { ErrorsCount } from '../../../src/models/ingestion/types';
+
+const defaultExpectedErrorsCount: ErrorsCount = {
+  vertices: 0,
+  metadata: 0,
+  geometryValidity: 0,
+  resolution: 0,
+  smallGeometries: 0,
+  smallHoles: 0,
+  unknown: 0,
+};
+
+export interface FailedValidationTestCase {
+  description: string;
+  shapefilePath: string;
+  expectedErrorsCount: ErrorsCount;
+  ppManagerValidationResult: PolygonPartsChunkValidationResult;
+}
+
+export const failedValidationTestCases: FailedValidationTestCase[] = [
+  {
+    description: 'Metadata error',
+    shapefilePath: '/invalid/metadata_error/invalid_publish_res_shapefile/ShapeMetadata.shp',
+    ppManagerValidationResult: {
+      parts: [],
+      smallHolesCount: 0,
+    },
+    expectedErrorsCount: { ...defaultExpectedErrorsCount, metadata: 1 },
+  },
+  {
+    description: 'Vertices error',
+    shapefilePath: '/invalid/vertices_error/3205_vertices/ShapeMetadata.shp',
+    ppManagerValidationResult: {
+      parts: [],
+      smallHolesCount: 0,
+    },
+    expectedErrorsCount: { ...defaultExpectedErrorsCount, vertices: 1 },
+  },
+  {
+    description: 'Geometry validity error(Self-intersecting)',
+    shapefilePath: '/invalid/geometry_validity_error/self_intersecting/ShapeMetadata.shp',
+    ppManagerValidationResult: {
+      parts: [{ id: '1', errors: ['Geometry_Validity'] }],
+      smallHolesCount: 0,
+    },
+    expectedErrorsCount: { ...defaultExpectedErrorsCount, geometryValidity: 1 },
+  },
+  {
+    description: 'Small geometries error',
+    shapefilePath: '/invalid/for_pp_invalid_mock/ShapeMetadata.shp',
+    ppManagerValidationResult: {
+      parts: [{ id: '1', errors: ['Small_Geometry'] }],
+      smallHolesCount: 0,
+    },
+    expectedErrorsCount: { ...defaultExpectedErrorsCount, smallGeometries: 1 },
+  },
+  {
+    description: 'Small holes error',
+    shapefilePath: '/invalid/for_pp_invalid_mock/ShapeMetadata.shp',
+    ppManagerValidationResult: {
+      parts: [{ id: '1', errors: ['Small_Holes'] }],
+      smallHolesCount: 100,
+    },
+    expectedErrorsCount: { ...defaultExpectedErrorsCount, smallHoles: 1 },
+  },
+  {
+    description: 'Resolution error',
+    shapefilePath: '/invalid/for_pp_invalid_mock/ShapeMetadata.shp',
+    ppManagerValidationResult: {
+      parts: [{ id: '1', errors: ['Resolution'] }],
+      smallHolesCount: 0,
+    },
+    expectedErrorsCount: { ...defaultExpectedErrorsCount, resolution: 1 },
+  },
+  {
+    description: 'Unknown error',
+    shapefilePath: '/invalid/for_pp_invalid_mock/ShapeMetadata.shp',
+    ppManagerValidationResult: {
+      parts: [{ id: '1', errors: ['Unknown'] }],
+      smallHolesCount: 0,
+    },
+    expectedErrorsCount: { ...defaultExpectedErrorsCount, unknown: 1 },
+  },
+  {
+    description: 'Multiple errors(metadata+vertices)',
+    shapefilePath: '/invalid/metadata_vertices_error/ShapeMetadata.shp',
+    ppManagerValidationResult: {
+      parts: [],
+      smallHolesCount: 50,
+    },
+    expectedErrorsCount: {
+      ...defaultExpectedErrorsCount,
+      metadata: 1,
+      vertices: 1,
+    },
+  },
+  {
+    description: 'Multiple errors(PolygonPartsManager errors)',
+    shapefilePath: '/invalid/for_pp_invalid_mock/ShapeMetadata.shp',
+    ppManagerValidationResult: {
+      parts: [{ id: '1', errors: ['Resolution', 'Small_Geometry', 'Small_Holes', 'Geometry_Validity', 'Unknown'] }],
+      smallHolesCount: 200,
+    },
+    expectedErrorsCount: {
+      ...defaultExpectedErrorsCount,
+      resolution: 1,
+      smallGeometries: 1,
+      geometryValidity: 1,
+      smallHoles: 1,
+      unknown: 1,
+    },
+  },
+  {
+    description: '2 metadata errors in 2 features',
+    shapefilePath: '/invalid/metadata_error/2_features_with_invalid_date/ShapeMetadata.shp',
+    ppManagerValidationResult: {
+      parts: [],
+      smallHolesCount: 0,
+    },
+    expectedErrorsCount: {
+      ...defaultExpectedErrorsCount,
+      metadata: 2,
+    },
+  },
+];
