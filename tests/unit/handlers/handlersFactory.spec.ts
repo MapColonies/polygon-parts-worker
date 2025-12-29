b02db8974dedcf3f906215690cd6c10a1ba19574
almog8k <almog8k@gmail.com>
2025-12-29 10:34:20 +0200

    fix: add logger mock to HandlerFactory tests

--- tests/unit/handlers/handlersFactory.spec.ts
+++ tests/unit/handlers/handlersFactory.spec.ts
@@ -1,11 +1,12 @@
 import * as fs from 'fs';
 import { BadRequestError } from '@map-colonies/error-types';
-import { HANDLERS } from '../../../src/common/constants';
+import { HANDLERS, SERVICES } from '../../../src/common/constants';
 import { initJobHandler } from '../../../src/models/handlerFactory';
 import { IngestionJobHandler } from '../../../src/models/ingestion/ingestionHandler';
 import { configMock } from '../mocks/configMock';
 import { registerExternalValues } from '../../../src/containerConfig';
 import { ingestionJobJobHandlerInstance } from '../jobProcessor/jobProcessorSetup';
+import { loggerMock } from '../mocks/telemetryMock';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-return
 jest.mock('fs', () => ({
@@ -24,7 +25,10 @@ describe('HandlerFactory', () => {
     (fs.accessSync as jest.Mock).mockImplementation(() => undefined); // simulate directories exist
 
     registerExternalValues({
-      override: [{ token: HANDLERS.NEW, provider: { useValue: ingestionJobJobHandlerInstance() } }],
+      override: [
+        { token: SERVICES.LOGGER, provider: { useValue: loggerMock } },
+        { token: HANDLERS.NEW, provider: { useValue: ingestionJobJobHandlerInstance() } },
+      ],
     });
   });
 
